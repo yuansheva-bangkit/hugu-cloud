e5e3cade9139e9d2d8f7c5b378d8c6f1312df54a
Ricca Amelia <126551668+rhikaaml@users.noreply.github.com>
2023-12-01 12:29:44 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,16 +2,41 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+const jwt = require('jsonwebtoken')
+// const dbConnection = require('./db')
 
 app.use(express.json())
 
-const usersRouter = require('./routes/users')
-app.use('/users', usersRouter)
+const posts = [
+  {
+    username: 'Kyle',
+    title: 'Post 1'
+  },
+  {
+    username: 'Jim',
+    title: 'Post 2'
+  }
+]
 
-app.listen(3000, () => console.log('Server Started'))
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
+})
+
+// app.get('/', (req, res) => {
+//   res.send('Selamat datang di proyek Express dengan MongoDB!')
+// })
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
+app.listen(3000)
