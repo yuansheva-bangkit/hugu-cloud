ee0cbf3ce57c72a7a65b3971851e46e25655062d
yuansheva-bangkit <151907158+yuansheva-bangkit@users.noreply.github.com>
2023-12-15 01:36:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(err, req, res, next) {
   res.json({ error: err });
 });
 
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
